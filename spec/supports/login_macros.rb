860963f1421e9384ac0fb038144b8823f19068f0
anonymous931 <fal825lere@gmail.com>
2023-03-21 14:38:18 +0900

    Add:ログイン、ログアウトのテストの追加

--- spec/supports/login_macros.rb
+++ spec/supports/login_macros.rb
@@ -1,9 +1,10 @@
 module LoginMacros
   def login_as(user)
     visit root_path
-    click_link 'Login'
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: 'password'
-    click_button 'Login'
+    find(".navbar-toggler-icon").click
+    click_link 'ログイン'
+    fill_in 'メールアドレス', with: user.email
+    fill_in 'パスワード', with: 'password'
+    click_button 'ログイン'
   end
 end
