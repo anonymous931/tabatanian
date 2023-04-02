d1b71026461fd65580ecd19117cf27143350ba29
anonymous931 <fal825lere@gmail.com>
2023-04-02 17:15:08 +0900

    Add:お気に入り機能の実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,12 +2,12 @@ class FavoritesController < ApplicationController
   def create
     menu = Menu.find(params[:menu_id])
     current_user.favorite(menu)
-    redirect_back_or_to root_path, success: t('defaults.message.register', items: Favorite.model_name.human )
+    redirect_back_or_to root_path, success: t('defaults.message.register', item: Favorite.model_name.human )
   end
 
   def destroy
     menu = current_user.favorites.find(params[:id]).menu
     current_user.unfavorite(menu)
-    redirect_back_or_to root_path, success: t('defaults.message.unregister', items: Favorite.model_name.human )
+    redirect_back_or_to root_path, success: t('defaults.message.unregister', item: Favorite.model_name.human )
   end
 end
