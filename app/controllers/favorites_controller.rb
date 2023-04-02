9ef79fc0cedfa8327101dbce45b747582f6033c2
anonymous931 <fal825lere@gmail.com>
2023-04-02 19:22:24 +0900

    Add:お気に入り機能を非同期で実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,11 @@
 class FavoritesController < ApplicationController
   def create
-    menu = Menu.find(params[:menu_id])
-    current_user.decorate.favorite(menu)
-    redirect_back fallback_location: root_path, success: t('defaults.message.register', item: Favorite.model_name.human)
+    @menu = Menu.find(params[:menu_id])
+    current_user.decorate.favorite(@menu)
   end
 
   def destroy
-    menu = current_user.favorites.find(params[:id]).menu
-    current_user.decorate.unfavorite(menu)
-    redirect_back fallback_location: root_path, success: t('defaults.message.unregister', item: Favorite.model_name.human)
+    @menu = current_user.favorites.find(params[:id]).menu
+    current_user.decorate.unfavorite(@menu)
   end
 end
