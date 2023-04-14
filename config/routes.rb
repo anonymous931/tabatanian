f7aaca77ad9bb83700f11a7d3d30dba031b375dc
anonymous931 <fal825lere@gmail.com>
2023-04-14 20:27:54 +0900

    Add:利用規約ページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -26,5 +26,7 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+
+  get 'terms', to: 'static_pages#terms'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
