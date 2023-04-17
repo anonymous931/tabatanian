ece94fdc0a5f46bb3e12131f6e419d5d30eee2fa
anonymous931 <fal825lere@gmail.com>
2023-04-17 20:47:04 +0900

    Add:プライバシーポリシーとロゴの実装

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,6 @@ class StaticPagesController < ApplicationController
   skip_before_action :require_login
 
   def terms; end
+
+  def policy; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -28,5 +28,6 @@ Rails.application.routes.draw do
   delete 'logout', to: 'user_sessions#destroy'
 
   get 'terms', to: 'static_pages#terms'
+  get 'policy', to: 'static_pages#policy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
