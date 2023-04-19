7c7b458ac6dbe75559e7e541ee2cc5791b2368d5
anonymous931 <fal825lere@gmail.com>
2023-04-20 04:41:35 +0900

    Fix:コメントを追加した際に、リロードしないと編集ボタンが反応しない問題を修正 理由としては動的に追加した要素は、イベントハンドラに登録されていないため、 要素そのものでなくdocumentを主体としてイベントを受け取ることで改善する

--- app/javascript/packs/comment_edit.js
+++ app/javascript/packs/comment_edit.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('.js-edit-comment-button').on("click",function(){
+  $(document).on("click", '.js-edit-comment-button', function(){
     const commentId = $(this).data('comment-id');
     const commentTextAreaCommentBox = $('#js-textarea-comment-box-' + commentId);
     commentTextAreaCommentBox.slideToggle();
