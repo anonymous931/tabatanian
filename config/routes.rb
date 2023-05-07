98427fb9a7c79dee0718bb54a2b1edafb832f67a
anonymous931 <fal825lere@gmail.com>
2023-05-07 19:10:08 +0900

    Add:aboutページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -29,5 +29,6 @@ Rails.application.routes.draw do
 
   get 'terms', to: 'static_pages#terms'
   get 'policy', to: 'static_pages#policy'
+  get 'about', to: 'static_pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
